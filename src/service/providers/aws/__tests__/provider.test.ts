ae3ee38fa09d8fee11b22aeaee30b576574fc6df
Mevan Samaratunga <msamaratunga@Mevans-MacBook-Pro.local>
2020-07-25 02:39:23 +0400

    updated with phone verification

--- src/service/providers/aws/__tests__/provider.test.ts
+++ src/service/providers/aws/__tests__/provider.test.ts
@@ -1,9 +1,13 @@
+import { promisify } from 'util';
 import path from 'path';
 
 import fetch from 'node-fetch';
 Object.defineProperty(global, 'fetch', {
   value: fetch
 });
+
+// const sleep = promisify(setTimeout);
+jest.setTimeout(60000);
 
 // these tests are done end-to-end and require
 // a pre-confgured cognito user pool accessible 
@@ -17,7 +21,7 @@ Object.defineProperty(global, 'fetch', {
 import 'crypto-js/lib-typedarrays';
 
 import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from '../../../../etc/aws-exports';
+import awsconfig from '../../../../../etc/aws-exports';
 Amplify.configure(awsconfig);
 
 // it appears USER_SRP_AUTH still fails when run within 
@@ -28,8 +32,8 @@ Auth.configure({
   authenticationFlowType: 'USER_PASSWORD_AUTH'
 })
 
-import { LOG_LEVEL_TRACE, setLogLevel } from '@appbricks/utils';
-import User from '../../../model/user';
+import { LOG_LEVEL_TRACE, setLogLevel, sleep } from '@appbricks/utils';
+import User from '../../../../model/user';
 import Provider from '../provider';
 
 let provider = new Provider(Auth);
@@ -40,8 +44,8 @@ let testType = process.env.TEST_TYPE || ''
 
 // gmail test account credentials
 const gmail = require("gmail-tester");
-const GMAIL_CREDS_PATH = '../../../../etc/gmail-creds.json';
-const GMAIL_AUTH_TOKEN_PATH = '../../../../.gmail-token';
+const GMAIL_CREDS_PATH = '../../../../../etc/gmail-creds.json';
+const GMAIL_AUTH_TOKEN_PATH = '../../../../../.gmail-token';
 
 // set log levels
 if (process.env.DEBUG) {
@@ -59,30 +63,32 @@ it('detects invalid initial session', async () => {
 // for the verification message
 let signUpTime = new Date();
 let signUpUser = 'appbricks-test-user-' + signUpTime.getTime();
+let verificationCode: string;
 
 it('registers a new user', async () => {
 
-  let user = new User()
-  user.username = signUpUser
-  user.password = '@ppBr!cks2020'
-  user.firstName = 'John'
-  user.familyName = 'Doe'
-  user.emailAddress = 'test.appbricks@gmail.com'
-  user.mobilePhone = '+17165755305'
+  let user = new User();
+  user.username = signUpUser;
+  user.password = '@ppBr!cks2020';
+  user.firstName = 'John';
+  user.familyName = 'Doe';
+  user.emailAddress = 'test.appbricks@gmail.com';
+  user.mobilePhone = '+17165755305';
 
   await provider.signUp(user)
     .then(userConfirmed => expect(userConfirmed).toBeFalsy())  
+
+  // pause to allow propagation of
+  // verification email to test email
+  // account
+  await sleep(10000);
 });
 
 it('detects new user is not confirmed during sign-in', async () => {
-  let user = new User()
-  user.username = signUpUser
-  user.password = '@ppBr!cks2020'
-  await provider.signIn(user)
-    .catch(err => expect(err).toEqual('notConfirmed'))
+  await provider.signIn(signUpUser, '@ppBr!cks2020')
+    .catch(err => expect(err).toEqual('notConfirmed'));
 });
 
-let verificationCode: string;
 it('receives an email with a verification code', async () => { 
 
   const email = await gmail.check_inbox(
@@ -122,25 +128,53 @@ it('confirms newly registered user', async () => {
   let user = new User();
   user.username = signUpUser;
   await provider.confirmSignUpCode(user, verificationCode)
-    .then(user => expect(user.username).toEqual(signUpUser));
+    .then(confirm => expect(confirm).toBeTruthy());
 });
 
 it('attempts to sign-in with an invalid password', async () => {
   let user = new User();
-  user.username = signUpUser;
-  user.password = 'invalid';
-  await provider.signIn(user)
+  await provider.signIn(signUpUser, 'invalid')
     .catch(err => expect(err).toEqual('invalidLogin'));
 });
 
 it('successfully signs-in', async () => {
   let user = new User();
-  user.username = signUpUser;
-  user.password = '@ppBr!cks2020';
-  await provider.signIn(user)
+  await provider.signIn(signUpUser, '@ppBr!cks2020')
     .then(authType => expect(authType).toEqual(0));
 });
 
-// it('initiates verification of the new user\'s mobile phone', async () => {
-//   await provider.sendVerificationCodeForAttribute('phone_number');
-// });
+it('initiates verification of the new user\'s mobile phone', async () => {
+  
+  await provider.sendVerificationCodeForAttribute('phone_number');
+
+  // pause to allow propagation of
+  // SMS to test email account
+  await sleep(10000);
+});
+
+it('verifies the code receive for validating the mobile phone', async () => {
+
+  const email = await gmail.check_inbox(
+    path.resolve(__dirname, GMAIL_CREDS_PATH),
+    path.resolve(__dirname, GMAIL_AUTH_TOKEN_PATH),
+    {
+      subject: "New text message from",
+      wait_time_sec: 10,
+      max_wait_time_sec: 60,
+      after: signUpTime,
+      include_body: true
+    }
+  );
+
+  expect(email).toBeDefined();
+  expect(email.receiver).toEqual('test.appbricks@gmail.com');
+  console.log("Email with text message!", email);
+
+  let bodyPattern = /Your verification code is ([0-9]+)/;
+  let matches = bodyPattern.exec(email.body.html);
+  expect(matches).toBeDefined();
+  expect(matches!.length).toBeGreaterThan(0);
+  verificationCode = matches![1];
+
+  await provider.confirmVerificationCodeForAttribute('phone_number', verificationCode);
+})
