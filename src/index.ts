aa5950c23319b9181b9b5b80848218c73c572b0d
Mevan Samaratunga <msamaratunga@Mevans-MacBook-Pro.local>
2020-08-20 15:23:56 -0400

    updated exports

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import User from './model/user';
+export { User };
+
+import AuthService from './service/auth-service'
+export { AuthService };
+
+import { AuthActionProps } from './service/action';
+export { AuthActionProps };
+
+import { AuthUserStateProp, AuthUserState } from './service/state';
+export { AuthUserStateProp, AuthUserState };
