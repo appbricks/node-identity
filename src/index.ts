dd0c73ca9a63e59d4e052a862606b708b8ce2de7
Mevan Samaratunga <mevansam@gmail.com>
2021-05-11 09:03:52 -0400

    cleaned up exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import {
+export * from './service/constants';
+
+export {
   SIGN_UP_REQ,
   RESEND_SIGN_UP_CODE_REQ,
   CONFIRM_SIGN_UP_CODE_REQ,
@@ -17,89 +19,12 @@ import {
   READ_USER_REQ
 } from './service/action';
 
-export {
-  SIGN_UP_REQ,
-  RESEND_SIGN_UP_CODE_REQ,
-  CONFIRM_SIGN_UP_CODE_REQ,
-  RESET_PASSWORD_REQ,
-  UPDATE_PASSWORD_REQ,
-  LOAD_AUTH_STATE_REQ,
-  SIGN_IN_REQ,
-  VALIDATE_MFA_CODE_REQ,
-  SIGN_OUT_REQ,
-  VERIFY_ATTRIBUTE_REQ,
-  CONFIRM_ATTRIBUTE_REQ,
-  CONFIGURE_MFA_REQ,
-  SETUP_TOTP_REQ,
-  VERIFY_TOTP_REQ,
-  SAVE_USER_REQ,
-  READ_USER_REQ
-}
-
-import {
-  AUTH_NO_MFA,
-  AUTH_MFA_SMS,
-  AUTH_MFA_TOTP,
-  ERROR_SIGN_UP,
-  ERROR_RESEND_SIGN_UP_CODE,
-  ERROR_CONFIRM_SIGN_UP_CODE,
-  ERROR_CONFIGURE_MFA,
-  ERROR_RESET_PASSWORD,
-  ERROR_UPDATE_PASSWORD,
-  ERROR_SIGN_IN,
-  ERROR_VALIDATE_MFA_CODE,
-  ERROR_SIGN_OUT,
-  ERROR_SEND_VERIFICATION_CODE_FOR_ATTRIBUTE,
-  ERROR_CONFIRM_VERIFICATION_CODE_FOR_ATTRIBUTE,
-  ERROR_READ_USER,
-  ERROR_SAVE_USER,
-  ERROR_NOT_VERIFIED,
-  ERROR_NOT_CONFIRMED,
-  ERROR_INVALID_LOGIN,
-  ERROR_INVALID_CODE,
-  ATTRIB_EMAIL_ADDRESS,
-  ATTRIB_MOBILE_PHONE
-} from './service/constants';
-
-export {
-  AUTH_NO_MFA,
-  AUTH_MFA_SMS,
-  AUTH_MFA_TOTP,
-  ERROR_SIGN_UP,
-  ERROR_RESEND_SIGN_UP_CODE,
-  ERROR_CONFIRM_SIGN_UP_CODE,
-  ERROR_CONFIGURE_MFA,
-  ERROR_RESET_PASSWORD,
-  ERROR_UPDATE_PASSWORD,
-  ERROR_SIGN_IN,
-  ERROR_VALIDATE_MFA_CODE,
-  ERROR_SIGN_OUT,
-  ERROR_SEND_VERIFICATION_CODE_FOR_ATTRIBUTE,
-  ERROR_CONFIRM_VERIFICATION_CODE_FOR_ATTRIBUTE,
-  ERROR_READ_USER,
-  ERROR_SAVE_USER,
-  ERROR_NOT_VERIFIED,
-  ERROR_NOT_CONFIRMED,
-  ERROR_INVALID_LOGIN,
-  ERROR_INVALID_CODE,
-  ATTRIB_EMAIL_ADDRESS,
-  ATTRIB_MOBILE_PHONE
-}
-
 import User from './model/user';
 export { User };
 
+export { AuthActionProps } from './service/action';
+export { AuthStateProps } from './service/state';
+
 import AuthService from './service/auth-service'
-export { AuthService };
-
-import { AuthActionProps } from './service/action';
-export { AuthActionProps };
-
-import { AuthStateProps, AuthState } from './service/state';
-export { AuthStateProps, AuthState as AuthUserState };
-
-import Provider from './service/provider';
-export { Provider }
-
 import AwsProvider from './service/providers/aws/provider';
-export { AwsProvider }
+export { AuthService, AwsProvider };
